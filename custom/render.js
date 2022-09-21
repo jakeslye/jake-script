3863a5cb0b14dccc29d35c59c807da2fb6644b51
Jake Slye <61893488+jakeslye@users.noreply.github.com>
2022-09-21 11:02:32 -0700

    added library

--- custom/render.js
+++ custom/render.js
@@ -1,6 +1,8 @@
 var c;
 var ctx;
 
+var render = new Map();
+
 function setup(peramiters){
     $("#console").append(`<div aria-disabled="true" style="text-align: center;">
                             <canvas id="canvas" width="200" height="100" style="border:1px solid #000000;"></canvas>
@@ -8,6 +10,7 @@ function setup(peramiters){
     c = document.getElementById("canvas");
     ctx = c.getContext("2d");
 }
+render.set("render.setup", setup);
 
 function draw(peramiters){
     ctx.beginPath();
@@ -15,6 +18,6 @@ function draw(peramiters){
     ctx.rect(peramiters[0], peramiters[1], peramiters[2], peramiters[3]);
     ctx.fill();
 }
+render.set("render.draw", setup);
 
-registerFunction("render.setup", setup);
-registerFunction("render.draw", draw);
+registerLibrary("render", render);
